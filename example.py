e69d6cb79bdaf412f7c38c2c72000ba7243e1545
san <kimsan0622@gmail.com>
2021-10-08 10:51:21 +0900

    update README.md

--- example.py
+++ example.py
@@ -1,33 +1,17 @@
-## 1. Reinstall encoder
-```shell
-cd encoder
-python3 setup.py install
-```
+# Copyright 2021 san kim
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-## 2. Reinstall decoder
-```shell
-cd decoder
-python3 setup.py install
-```
-
-## Install dep
-
-### from Advenced Package Tool
-
-```bash
-sudo apt-get install -y libavutil-dev libavcodec-dev libavformat-dev libswscale-dev
-```
-
-### from source
-
-```bash
-# for python 3
-bash install_dep.sh
-```
-
-## Sample code
-
-```python
 import cv2
 
 import videoencoder
@@ -66,6 +50,4 @@ try:
         key = cv2.waitKey(int(1000/framerate))
 except KeyboardInterrupt:
     exit()
-```
-
 
